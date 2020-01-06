864b8a36218f8471280feeefcc27f422deb62676
tabatak <hamamoto.kazuto@gmail.com>
2020-01-06 22:48:49 +0900

    ABC093-C
    パリティの勉強が必要。というかこの辺の素養が必要。
    2ずつ加算されるから、合計値のパリティは変化しない！

---
+++ src/main/scala/abc094/c/Main.scala
@@ -0,0 +1,24 @@
+package abc094.c
+
+import scala.math.max
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    solve
+  }
+
+  def solve(): Unit = {
+    val sc = new java.util.Scanner(System.in)
+    val a, b, c = sc.nextInt
+
+    val mx = max(max(a, b), c)
+    val m = if (3 * mx % 2 == (a + b + c) % 2) {
+      mx
+    } else {
+      mx + 1
+    }
+
+    val ans = ((3 * m) - (a + b + c)) / 2
+    println(ans)
+  }
+}
