e734c60b25f3a2828102ff53b0d26b0366aa4058
tabatak <hamamoto.kazuto@gmail.com>
2020-01-20 21:05:13 +0900

    ABC152-D
    冷静になって数え上げられれば解けた問題。
    とりあえず数え上げてみるってのは大事かもしれない。

--- src/main/scala/abc152/d/Main.scala
+++ src/main/scala/abc152/d/Main.scala
@@ -5,12 +5,36 @@ object Main {
     solve
   }
 
+
   def solve(): Unit = {
     val sc = new java.util.Scanner(System.in)
-    val n = sc.nextLong
-    val keta = n.toString.length
-    var count = 0
-    println(keta)
+    val n = sc.nextInt
 
+    val frequency = scala.collection.mutable.Map[(Int, Int), Int]()
+    for (i <- 1 to n) {
+      val f = getFreqKey(i)
+      if (frequency.contains(f)) frequency(f) += 1
+      else frequency.put(f, 1)
+    }
+
+    var ans = 0
+    for (i <- 1 to n) {
+      val tmp = getFreqKey(i)
+      val f = (tmp._2, tmp._1)
+      if (frequency.contains(f)) ans += frequency(f)
+    }
+
+    println(ans)
+  }
+
+  def getFreqKey(x: Int): (Int, Int) = {
+    val a = x % 10
+    var b = 0
+    var tmp = x
+    while (tmp != 0) {
+      b = tmp
+      tmp = tmp / 10
+    }
+    (a, b)
   }
 }
