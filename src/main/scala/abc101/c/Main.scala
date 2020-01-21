120790fb6db9613d40c956c65cddb84c95fde357
tabatak <hamamoto.kazuto@gmail.com>
2020-01-21 22:31:02 +0900

    ABC101-C
    わかると簡単なんだけど、わからない。

--- src/main/scala/abc101/c/Main.scala
+++ src/main/scala/abc101/c/Main.scala
@@ -5,17 +5,6 @@ object Main {
     solve
   }
 
-  def getDivCount(x: Int, divisor: Int): Int = {
-    // ここの数え方を考える
-    var tmp = x
-    var count = 0
-    while (tmp > 0) {
-      count += 1
-      tmp = tmp - divisor
-    }
-    count
-  }
-
   def solve(): Unit = {
     val sc = new java.util.Scanner(System.in)
     val n, k = sc.nextInt
@@ -24,7 +13,6 @@ object Main {
       a(i) = sc.nextInt
     }
 
-    val minIndex = a.indexOf(1)
-    println(getDivCount(minIndex, k-1) + getDivCount(n - minIndex - 1, k-1))
+    println((n - 1 - 1) / (k - 1) + 1)
   }
 }
