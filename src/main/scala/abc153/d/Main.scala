497d9dd8b78005d64d35949600fd3ba2523cce83
tabatak <hamamoto.kazuto@gmail.com>
2020-01-26 22:46:44 +0900

    ABC153-D
    まちがってEの回答をこっちに書いてしまっていた。

--- src/main/scala/abc153/d/Main.scala
+++ src/main/scala/abc153/d/Main.scala
@@ -7,27 +7,19 @@ object Main {
 
   def solve(): Unit = {
     val sc = new java.util.Scanner(System.in)
-    var h, n = sc.nextInt
-    val abc = new Array[(Long, Long, Double)](n)
-    var max = -1L
-    var min = Long.MaxValue
-    for (i <- 0 until n) {
-      val a = sc.nextLong
-      val b = sc.nextLong
-      val c = a.toDouble / b. toDouble  //効率的な値
-      abc(i) = (a, b, c)
-      max = math.max(max, a)
-      min = math.min(min, a)
-    }
-    val sorted = abc.sortBy(_._3).reverse
-    println(sorted.toList)
+    var h = sc.nextLong
 
-    var mp = 0
-    while(h > 0) {
-      for(v <- abc) {
-        
-      }
+    var count = 0
+    while (h > 1) {
+      count += 1
+      h = h / 2
     }
+
+    var num = 0L
+    for (i <- 0 to count) {
+      num += math.pow(2, i).toLong
+    }
+    println(num)
 
   }
 }
