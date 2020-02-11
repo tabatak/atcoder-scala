1fadbd31673bf97924390d066bb72bed09645532
tabatak <hamamoto.kazuto@gmail.com>
2020-02-11 09:30:48 +0900

    2番目をとっておくだけでよかった。。。でも処理時間は増えた。。(941ms -> 1041ms)

--- src/main/scala/abc134/c/Main.scala
+++ src/main/scala/abc134/c/Main.scala
@@ -2,8 +2,29 @@ package abc134.c
 
 object Main {
   def main(args: Array[String]): Unit = {
-    solve
+    solve2
   }
+
+  def solve2(): Unit = {
+    val sc = new java.util.Scanner(System.in)
+    val n = sc.nextInt
+    val a = new Array[Int](n)
+    for (i <- 0 until n) {
+      a(i) = sc.nextInt
+    }
+
+    //ryota さんのコードを見てなるほどなと
+    val sorted = a.sorted
+    val max = sorted(n - 1)
+    val secondMax = sorted(n - 2)
+    val pw = new java.io.PrintWriter(System.out)
+    for (i <- 0 until n) {
+      if (a(i) == max) pw.println(secondMax)
+      else pw.println(max)
+    }
+    pw.flush()
+  }
+
 
   def solve(): Unit = {
     val sc = new java.util.Scanner(System.in)
