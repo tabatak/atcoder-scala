a430d194ece7096f67d9642c298dc01ffb7c53d1
tabatak <hamamoto.kazuto@gmail.com>
2020-02-17 13:20:25 +0900

    ABC146-C (解説AC だと思う)

---
+++ src/main/scala/abc146/c/Main.scala
@@ -0,0 +1,39 @@
+package abc146.c
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    solve
+  }
+
+  def solve(): Unit = {
+    val sc = new java.util.Scanner(System.in)
+    val a, b, x = sc.nextLong
+
+    def binarySearch(l: Long, r: Long): Long = {
+      val mid = (l + r) / 2
+
+      if (checkCost(mid)) {
+        // 中間値がコスト内で購入できる場合、より大きな整数を試す
+        if (mid + 1 == r) return mid
+        binarySearch(mid, r)
+      } else {
+        // 中間値がコスト内で購入できない場合、より小さな整数を試す
+        binarySearch(l, mid)
+      }
+    }
+
+    def checkCost(n: Long): Boolean = {
+      if (n == 0) false
+      else a * n + b * n.toString.length <= x
+    }
+
+    if (checkCost(1000000000L)) {
+      println(1000000000L)
+    } else {
+      println(binarySearch(0, 1000000000L))
+    }
+
+
+  }
+}
+
