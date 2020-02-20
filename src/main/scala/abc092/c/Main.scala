bc18565253b7f9a1e64e96861d47a3e871d0c378
tabatak <hamamoto.kazuto@gmail.com>
2020-02-20 22:00:39 +0900

    ABC092-C (解説を見てから写経)
    なるほどな。だけど、相変わらずindexがごちゃついている。

--- src/main/scala/abc092/c/Main.scala
+++ src/main/scala/abc092/c/Main.scala
@@ -1,32 +1,61 @@
 package abc092.c
 
-object Main extends App {
-  val sc = new java.util.Scanner(System.in)
-  val n = sc.nextInt
-  var a = new Array[Int](n + 2)
-  a(0) = 0
-  a(n + 1) = 0
-  for (i <- 1 until n + 1) {
-    a(i) = sc.nextInt()
+object Main {
+  def main(args: Array[String]): Unit = {
+    solve2
   }
-  val accL = new Array[Int](n + 2)
-  for (i <- 1 until n + 2) {
-    accL(i) = accL(i - 1) + math.abs(a(i) - a(i - 1))
-  }
-  var accR = new Array[Int](n + 2)
-  val reverseA = a.reverse
-  for (i <- 1 until n + 2) {
-    accR(i) = accR(i - 1) + math.abs(reverseA(i) - reverseA(i - 1))
-  }
-  accR = accR.reverse
 
-  //indexやら、reverseやら全然整理がつかん。。
-  val pw = new java.io.PrintWriter(System.out)
-  for (i <- 1 until n + 1) {
-    val cost = accL(i - 1) + math.abs(a(i - 1) - a(i + 1)) + accR(i + 1)
-    pw.println(cost)
+  def solve2(): Unit = {
+    val sc = new java.util.Scanner(System.in)
+    val n = sc.nextInt
+    val a = new Array[Int](n + 2) // 両端は0にする
+    for (i <- 1 until n + 1) {
+      a(i) = sc.nextInt()
+    }
 
+    var cost = 0
+    for (i <- 1 until n + 2) {
+      cost += math.abs(a(i) - a(i - 1))
+    }
+    val pw = new java.io.PrintWriter(System.out)
+    for (i <- 1 until n + 1) {
+      val tmp = cost - math.abs(a(i - 1) - a(i)) - math.abs(a(i) - a(i + 1)) + math.abs(a(i - 1) - a(i + 1))
+      pw.println(tmp)
+    }
+    pw.flush()
   }
-  pw.flush()
+
+
+  // 以下は最初に出したややこしすぎるやり方。。
+  def solve(): Unit = {
+    val sc = new java.util.Scanner(System.in)
+    val n = sc.nextInt
+    var a = new Array[Int](n + 2)
+    a(0) = 0
+    a(n + 1) = 0
+    for (i <- 1 until n + 1) {
+      a(i) = sc.nextInt()
+    }
+    val accL = new Array[Int](n + 2)
+    for (i <- 1 until n + 2) {
+      accL(i) = accL(i - 1) + math.abs(a(i) - a(i - 1))
+    }
+    var accR = new Array[Int](n + 2)
+    val reverseA = a.reverse
+    for (i <- 1 until n + 2) {
+      accR(i) = accR(i - 1) + math.abs(reverseA(i) - reverseA(i - 1))
+    }
+    accR = accR.reverse
+
+    //indexやら、reverseやら全然整理がつかん。。
+    val pw = new java.io.PrintWriter(System.out)
+    for (i <- 1 until n + 1) {
+      val cost = accL(i - 1) + math.abs(a(i - 1) - a(i + 1)) + accR(i + 1)
+      pw.println(cost)
+
+    }
+    pw.flush()
+  }
+
 
 }
