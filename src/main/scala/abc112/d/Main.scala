074fa7013450ffc728ee21dbfea33f5de659cd3a
tabatak <hamamoto.kazuto@gmail.com>
2020-02-24 14:11:38 +0900

    ABC112-D (ryotaさんの解答を見てかいた)
    こっちの方がわかりやすくはある。
    最大値が見つかればそれで終了でいいわけだし。

--- src/main/scala/abc112/d/Main.scala
+++ src/main/scala/abc112/d/Main.scala
@@ -2,7 +2,7 @@ package abc112.d
 
 object Main {
   def main(args: Array[String]): Unit = {
-    solve
+    solve2
   }
 
   def solve(): Unit = {
@@ -18,4 +18,18 @@ object Main {
     }
     println(ans)
   }
+
+  def solve2(): Unit = {
+    val sc = new java.util.Scanner(System.in)
+    val n, m = sc.nextInt()
+
+    def check(k: Int): Int = {
+      if (m % k == 0) return k
+      else check(k - 1)
+    }
+
+    // M >= K * N
+    println(check(m / n))
+  }
+
 }
