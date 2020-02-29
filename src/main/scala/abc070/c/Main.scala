19aea688e1ec43652931c37d1918d47ea8ca2e1a
tabatak <hamamoto.kazuto@gmail.com>
2020-02-29 21:30:20 +0900

    ABC070-C (自力AC)
    最大公倍数を求めるだけ。Longのオーバーフローで1WA。
    先にGCDで割ればいいのかな。

---
+++ src/main/scala/abc070/c/Main.scala
@@ -0,0 +1,39 @@
+package abc070.c
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    solve
+  }
+
+  def solve(): Unit = {
+    val sc = new java.util.Scanner(System.in)
+    val n = sc.nextInt
+    val ts = new Array[Long](n)
+    for (i <- 0 until n) {
+      ts(i) = sc.nextLong
+    }
+
+    var ans = ts(0)
+    for (i <- 1 until n) {
+      ans = lcm(ans, ts(i))
+    }
+
+    println(ans)
+  }
+
+  def lcm(x: Long, y: Long): Long = {
+    if (x % y == 0) {
+      x
+    } else if (y % x == 0) {
+      y
+    } else {
+      x / gcd(x, y) * y
+    }
+  }
+
+  def gcd(x: Long, y: Long): Long = {
+    if (x == 0) y
+    else gcd(y % x, x)
+  }
+
+}
